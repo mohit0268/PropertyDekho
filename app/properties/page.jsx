62d2eb61e8b0285223d0d241d700f8a83ee8fe9f
mohit purohit <65717412+mohit0268@users.noreply.github.com>
2024-07-29 14:16:11 +0530

    added the property card inside property page

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import PropertyCard from '@/components/PropertyCard'
+import properties from '@/properties.json'
 
-const Property = () => {
+
+const PropertyPage = () => {
   return (
-    <div>Property Page</div>
+    <section>
+      <div className='container lg:container m-auto'>
+
+        {properties.length === 0 ? "No Properties Found!" : (
+          
+          <div className='grid grid-cols-1 gap-4 md:grid-cols-3 p-4 rounded-lg'>
+            {properties.map(property => (
+              <PropertyCard key={property._id} property={property} />
+            ))}
+          </div>
+        )}
+      
+      </div>
+    </section>
   )
 }
 
-export default Property
+export default PropertyPage
