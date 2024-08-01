fe4187bc034add33bf20bd81296e6c9ce564f39d
Mohit Purohit <mohitpurohit020@gmail.com>
2024-08-01 16:25:55 +0530

    added page loader code

--- app/loading.jsx
+++ app/loading.jsx
@@ -1,7 +1,20 @@
-const pageLoader = () => {
+'use client'
+import ClipLoader from 'react-spinners/ClipLoader';
+
+const Override = {
+    display:'block',
+    margin:'100px auto',
+}
+
+const pageLoader = ({loading}) => {
   return (
-    <>
-    </>
+    <ClipLoader
+    color = {"#3b82f6"}
+    loading={loading}
+    cssOverride={Override}
+    size={150}
+    aria-label="Loading spinner"
+    data-testid = "loader"/>
   )
 }
 
